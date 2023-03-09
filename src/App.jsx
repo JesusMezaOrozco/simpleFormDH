dc482e45b5c3e90fea27d04980a9f25c31640f81
JesusMezaOrozco <jesus.orozco@3dves.com>
2023-03-08 20:19:46 -0500

    WIP: finish first version of the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,105 @@
+import { useState } from 'react'
+import Card from './Card'
+
+import './App.css'
+
 function App() {
 	//Aqui deberias agregar los estados y los handlers para los inputs
+	const [formState, setFormState] = useState({
+		userName: '',
+		favoritePetName: '',
+	})
+	const { userName, favoritePetName } = formState
+
+	const [showCard, setShowCard] = useState(false)
+	const [showAlert, setShowAlert] = useState(false)
+
+	const handleFormData = (event) => {
+		const { name, value } = event.target
+		setFormState({
+			...formState,
+			[name]: value,
+		})
+	}
+
+	const handleReset = () => {
+		setFormState({
+			userName: '',
+			favoritePetName: '',
+		})
+		setShowCard(false)
+		setShowAlert(false)
+	}
+
+	const validateForm = () => {
+		if (
+			userName.includes(' ') ||
+			userName.length < 3 ||
+			favoritePetName.length < 6
+		) {
+			return true
+		} else {
+			return false
+		}
+	}
+
+	const submitFormData = (event) => {
+		event.preventDefault()
+		if (!validateForm()) {
+			setShowAlert(false)
+			setShowCard(!showCard)
+		} else {
+			setShowAlert(true)
+		}
+	}
 
 	return (
-		<div className='App'>
-			<h1>Elige un color</h1>
-			<form>{/* aqui deberias escribir tu codigo */}</form>
+		<div>
+			<div className='form-container'>
+				<h1>Dejame Conocerte Mejor</h1>
+				<form onSubmit={submitFormData}>
+					<div className='input-container'>
+						<label htmlFor='user-name'>Como te llamas?</label>
+						<input
+							id='user-name'
+							type='text'
+							name='userName'
+							value={userName}
+							autoComplete='off'
+							placeholder='Escribe tu nombre aqui!'
+							minLength={3}
+							onChange={handleFormData}
+						/>
+					</div>
+					<div className='input-container'>
+						<label htmlFor='user-favorite-pet'>
+							Cual es tu mascota preferida?
+						</label>
+						<input
+							id='user-favorite-pet'
+							type='text'
+							name='favoritePetName'
+							autoComplete='off'
+							value={favoritePetName}
+							placeholder='Escribelo aqui!'
+							minLength={6}
+							onChange={handleFormData}
+						/>
+					</div>
+					<div>
+						<button type='submit'>Enviar</button>
+						<button onClick={handleReset}>Reset</button>
+					</div>
+				</form>
+			</div>
+			<div className='card-container'>
+				{showCard && (
+					<Card userName={userName} favoritePetName={favoritePetName} />
+				)}
+				{showAlert && (
+					<h3>Por favor chequea que la información sea correcta</h3>
+				)}
+			</div>
 		</div>
 	)
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,12 +1,13 @@
 //Este componente deberia recibir por props y mostrar en pantalla la informacion
 //que envia el usuario
 
-function Card() {
-  return (
-    <div>
-      <h2>Esto es un componente</h2>
-    </div>
-  );
+function Card(props) {
+	return (
+		<div>
+			<h1>Hola {props.userName} !</h1>
+			<h2>Tu mascota preferida es: {props.favoritePetName}</h2>
+		</div>
+	)
 }
 
-export default Card;
+export default Card
